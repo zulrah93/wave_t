ef6f425156e81635aa9661e203001adc2d54904e
Daniel Lopez <dlope073@gmail.com>
2025-07-10 13:21:09 -0400

    Add wave generator functions to generate test signals

--- example/main.cpp
+++ example/main.cpp
@@ -1,32 +1,8 @@
 #include <cstdint>
 #include <wave_t.hpp>
 #include <iostream>
-#include <numbers>
-#include <cmath>
-#include <limits>
 
 /* Example using the header only wave file reader and writer class in wave_t.hpp */
-
-// Source: https://en.wikipedia.org/wiki/Sine_wave
-inline int16_t pcm_sine(double frequency, double time, double amplititude) {
-    return static_cast<int16_t>(amplititude * sin(2 * std::numbers::pi * frequency * time));
-}
-
-// Source: https://en.wikipedia.org/wiki/Triangle_wave
-inline int16_t pcm_triangle_wave(double time, double amplititude, double frequency) {
-  const double period = (1.0 / frequency);
-  return static_cast<int16_t>(fabs(((2.0 * amplititude) / std::numbers::pi) * asin(sin(2 * time * (std::numbers::pi / period )))));
-}
-
-inline double set_volume(double percent) {
-   if (percent > 1.0) {
-      percent = 1.0;
-   }
-   if (percent < 0.0) {
-      percent = 0.0;
-   }
-   return static_cast<double>(INT16_MAX - 1024) * percent; 
-}
 
 int main(int arguments_size, char** arguments) {
   std::cout << "wave_t.hpp usage example!" << std::endl;
@@ -34,15 +10,9 @@ int main(int arguments_size, char** arguments) {
   output.set_sample_rate(44100);
   output.set_number_of_channels(1);
   output.set_bits_per_sample(16);
-  const size_t sample_size = 44100 * 60 * 5; // 5 minutes of 16-bit PCM samples
-  double time = std::numbers::pi;
-  const double frequency = 440.0;
-  for(size_t _ = 0; _ < sample_size; _++) {
-      int16_t sample = pcm_triangle_wave(time, set_volume(0.35), frequency);
-      sample += pcm_sine(frequency, time, set_volume(0.35));
-      output.add_16_bits_sample(sample);
-      time += 0.00001;
-  }
+  const size_t sample_size = 44100 * 60 * 5; // 5 minutes of 16-bit PCM sample
+  //This helper member function can generate one or a combination of waves only supports mono or stereo for now 
+  output.generate_wave(wave_type_t::sine | wave_type_t::triangle | wave_type_t::square, sample_size, 440.0, 0.8);
   output.save("output.wav");
   return 0;
 }
--- include/wave_t.hpp
+++ include/wave_t.hpp
@@ -1,13 +1,18 @@
 #ifndef WAVE_T_HPP
 #define WAVE_T_HPP
 
+#include <bit>
 #include <cstdint>
 #include <cstdio>
 #include <cstring>
-#include <string>
+#include <functional>
 #include <stdarg.h>
+#include <string>
 #include <vector>
-#include <bit>
+#include <numbers>
+#include <cmath>
+#include <limits>
+
 
 constexpr auto RIFF_ASCII = std::byteswap(0x52494646);
 constexpr auto WAVE_ASCII = std::byteswap(0x57415645);
@@ -17,7 +22,9 @@ constexpr auto PCM = 1;
 constexpr auto _8_BITS_PER_SAMPLE = 8;
 constexpr auto _16_BITS_PER_SAMPLE = 16;
 constexpr auto _24_BITS_PER_SAMPLE = 24;
-constexpr auto DEFAULT_RESERVE_VALUE = 44100*60*5;
+constexpr auto DEFAULT_RESERVE_VALUE = 44100 * 60 * 5;
+
+enum wave_type_t : uint8_t { sine = 1, triangle = 2, square = 4 };
 
 struct wave_header_t {
   uint32_t chunk_id;
@@ -64,8 +71,10 @@ public:
       va_start(args, sample);
     }
     m_samples.push_back(sample);
-    for (size_t _ = 1; m_header.number_of_channels > 1 && _ < m_header.number_of_channels; _++) {
-        m_samples.push_back(va_arg(args, const int32_t));
+    for (size_t _ = 1;
+         m_header.number_of_channels > 1 && _ < m_header.number_of_channels;
+         _++) {
+      m_samples.push_back(va_arg(args, const int32_t));
     }
     if (m_header.number_of_channels > 1) {
       va_end(args);
@@ -79,8 +88,10 @@ public:
       va_start(args, sample);
     }
     m_samples.push_back(static_cast<int32_t>(sample));
-    for (size_t _ = 1; m_header.number_of_channels > 1 && _ < m_header.number_of_channels; _++) {
-        m_samples.push_back(static_cast<int32_t>(va_arg(args, const int16_t)));
+    for (size_t _ = 1;
+         m_header.number_of_channels > 1 && _ < m_header.number_of_channels;
+         _++) {
+      m_samples.push_back(static_cast<int32_t>(va_arg(args, const int16_t)));
     }
     if (m_header.number_of_channels > 1) {
       va_end(args);
@@ -94,13 +105,96 @@ public:
       va_start(args, sample);
     }
     m_samples.push_back(static_cast<int32_t>(sample));
-    for (size_t _ = 1; m_header.number_of_channels > 1 && _ < m_header.number_of_channels; _++) {
-        m_samples.push_back(static_cast<int32_t>(va_arg(args, const int8_t)));
+    for (size_t _ = 1;
+         m_header.number_of_channels > 1 && _ < m_header.number_of_channels;
+         _++) {
+      m_samples.push_back(static_cast<int32_t>(va_arg(args, const int8_t)));
     }
     if (m_header.number_of_channels > 1) {
       va_end(args);
     }
     return true;
+  }
+
+  
+  bool generate_wave(uint8_t wave_type, size_t sample_size,
+                     double frequency, double volume_percent) {
+
+      auto set_volume = [](double percent) {
+            if (percent > 1.0) {
+              percent = 1.0;
+            }
+            if (percent < 0.0) {
+              percent = 0.0;
+            }
+            return static_cast<double>(INT16_MAX - 1024) * percent;
+      };
+
+      //Source: https://en.wikipedia.org/wiki/Sign_function
+      auto sign = [](double x) {
+          if (x > 0) {
+              return 1.0;
+          }
+          else if (x < 0) {
+              return -1.0;
+          }
+          else {
+              return 0.0;
+          }
+      };
+
+      // Source: https://en.wikipedia.org/wiki/Sine_wave
+      auto pcm_sine = [](double _frequency, double time, double amplititude) {
+        return static_cast<int16_t>(
+            amplititude * sin(2 * std::numbers::pi * _frequency * time));
+      };
+
+      // Source: https://en.wikipedia.org/wiki/Triangle_wave
+      auto pcm_triangle = [](double time, double amplititude, double _frequency) {
+            const double period = (1.0 / _frequency);
+            return static_cast<int16_t>(fabs(((2.0 * amplititude) / std::numbers::pi) * asin(sin(2 * time * (std::numbers::pi / period )))));
+      };
+
+      // Source: https://en.wikipedia.org/wiki/Square_wave_(waveform)
+      auto pcm_square = [sign](double time, double amplititude, double _frequency) {
+            return static_cast<int16_t>(amplititude * sign(sin(2 * std::numbers::pi * _frequency * time)));
+      };
+
+      bool is_stereo = m_header.number_of_channels == 2;
+
+      double time = std::numbers::pi;
+      for (size_t _ = 0; _ < sample_size; _++) {
+        size_t wave_count = static_cast<size_t>((wave_type & wave_type_t::sine) == (wave_type_t::sine)) +
+                                static_cast<size_t>((wave_type & wave_type_t::triangle) == (wave_type_t::triangle)) +
+                                static_cast<size_t>((wave_type & wave_type_t::square) == (wave_type_t::square));
+        const double volume = set_volume(volume_percent / static_cast<double>(wave_count));
+        int16_t sample = 0;
+        if ((wave_type & wave_type_t::sine)) {
+          sample += pcm_sine(frequency, time, volume);
+        }
+        if ((wave_type & wave_type_t::triangle)) {
+          sample += pcm_triangle(time, volume, frequency);
+        }
+        if ((wave_type & wave_type_t::square)) {
+          sample += pcm_square(time, volume, frequency);
+        }
+        switch (m_header.bits_per_sample) {
+        case _8_BITS_PER_SAMPLE:
+          add_8_bits_sample(sample);
+          break;
+        case _16_BITS_PER_SAMPLE:
+          add_16_bits_sample(sample);
+          break;
+        case _24_BITS_PER_SAMPLE:
+          add_24_bits_sample(sample);
+          break;
+        default:
+          return false;
+        }
+        time += 0.00001;
+      }
+
+      return true;
   }
 
   bool save(const std::string &file_path) {
@@ -133,9 +227,9 @@ private:
     std::vector<int8_t> samples;
     samples.reserve(m_samples.size());
     for (auto &sample : m_samples) {
-        samples.push_back(static_cast<int8_t>(sample));
+      samples.push_back(static_cast<int8_t>(sample));
     }
-    
+
     auto wav_file_handle = fopen(file_path.c_str(), "wb");
     if (nullptr == wav_file_handle) {
       return false;
@@ -145,17 +239,17 @@ private:
     size_t written_bytes =
         fwrite(reinterpret_cast<int8_t *>(&m_header), sizeof(int8_t),
                sizeof(m_header), wav_file_handle);
-    written_bytes += fwrite(samples.data(), sizeof(int8_t), samples.size(),
-                            wav_file_handle);
+    written_bytes +=
+        fwrite(samples.data(), sizeof(int8_t), samples.size(), wav_file_handle);
     fclose(wav_file_handle);
     return written_bytes == expected_bytes;
   }
 
-  bool save_as_16_bits(const std::string &file_path) { 
+  bool save_as_16_bits(const std::string &file_path) {
     std::vector<int16_t> samples;
     samples.reserve(m_samples.size());
     for (auto &sample : m_samples) {
-        samples.push_back(static_cast<int16_t>(sample));
+      samples.push_back(static_cast<int16_t>(sample));
     }
 
     auto wav_file_handle = fopen(file_path.c_str(), "wb");
@@ -163,7 +257,8 @@ private:
       return false;
     }
 
-    const size_t expected_bytes = sizeof(m_header) + (sizeof(int16_t) * samples.size());
+    const size_t expected_bytes =
+        sizeof(m_header) + (sizeof(int16_t) * samples.size());
     size_t written_bytes =
         fwrite(reinterpret_cast<int8_t *>(&m_header), sizeof(int8_t),
                sizeof(m_header), wav_file_handle);
@@ -171,7 +266,6 @@ private:
                             wav_file_handle);
     fclose(wav_file_handle);
     return written_bytes == expected_bytes;
-
   }
 
   bool save_as_24_bits(const std::string &file_path) { return false; }
